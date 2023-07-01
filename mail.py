eaeb6779ef483ba3d51b5d8b79b329afaabdfb93
Anh Khoa Phan Le <dolphy.phanle@gmail.com>
2023-07-01 22:24:57 +0100

    use .env

--- mail.py
+++ mail.py
@@ -1,46 +1,9 @@
-# import base64
-# from email.mime.text import MIMEText
-# from google_auth_oauthlib.flow import InstalledAppFlow
-# from googleapiclient.discovery import build
-# from requests import HTTPError lmyzmctmrdydohdg
-
-
-# SCOPES = [
-#         "https://www.googleapis.com/auth/gmail.send"
-#     ]
-# flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
-# creds = flow.run_local_server(port=0)
-
-# service = build('gmail', 'v1', credentials=creds)
-# # message = MIMEText('This is the body of the email')
-# body = """
-# Hey y’all,
-
-# Please update the google doc for this Wednesday's game: https://tinyurl.com/1spatial
-# And check here for any debt: https://docs.google.com/spreadsheets/d/1ozb59B-EGwDfrgqMtm9dEeD34PM0Bff7jReJx9duENs/edit?usp=sharing 
-
-# Cheers,
-
-# Dolphy
-
-# """
-# message = MIMEText(body, 'html')
-# message['to'] = 'dolphy@aliases.me'
-# message['subject'] = 'Email Subject'
-# create_message = {'raw': base64.urlsafe_b64encode(message.as_bytes()).decode()}
-
-# try:
-#     message = (service.users().messages().send(userId="me", body=create_message).execute())
-#     print(F'sent message to {message} Message Id: {message["id"]}')
-# except HTTPError as error:
-#     print(F'An error occurred: {error}')
-#     message = None
-
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import os
 from datetime import datetime, timedelta
+from dotenv import load_dotenv
 
 def nextWednesday():
    # Get the current date
@@ -80,6 +43,8 @@ def send_email(subject, body, sender, recipients, password):
 
 if __name__ == "__main__":
 
+   load_dotenv()
+   
    subject = "[footy5aside] Game on " + nextWednesday()
    body = """
    <html>
@@ -97,5 +62,5 @@ if __name__ == "__main__":
    sender = "dolphy.phanle@gmail.com"
    recipients = ["dolphy@aliases.me"]
    # Get password from environment variable
-   password = os.environ.get("EMAIL_PASSWORD")
+   password = os.getenv('EMAIL_PASSWORD')
    send_email(subject, body, sender, recipients, password)
